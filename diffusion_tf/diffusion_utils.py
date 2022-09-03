3f4310526d9077df20d34272bb158e3559495350
Cywtim <47052542+Cywtim@users.noreply.github.com>
2022-09-03 19:06:47 +1000

    Update diffusion_utils.py

--- diffusion_tf/diffusion_utils.py
+++ diffusion_tf/diffusion_utils.py
@@ -38,10 +38,10 @@ def get_beta_schedule(beta_schedule, *, beta_start, beta_end, num_diffusion_time
   return betas
 
 
-def noise_like(shape, noise_fn=tf.random_normal, repeat=False, dtype=tf.float32):
+def noise_like(shape, noise_fn=tf.random_normal, repeat=False, dtype=tf.float32): 
   repeat_noise = lambda: tf.repeat(noise_fn(shape=(1, *shape[1:]), dtype=dtype), repeats=shape[0], axis=0)
   noise = lambda: noise_fn(shape=shape, dtype=dtype)
-  return repeat_noise() if repeat else noise()
+  return repeat_noise() if repeat else noise() # generate noise if need repeat
 
 
 class GaussianDiffusion:
@@ -99,31 +99,39 @@ class GaussianDiffusion:
     return tf.reshape(out, [bs] + ((len(x_shape) - 1) * [1])) # reshape to [batch_size, 1, 1, 1, 1, ...]
 
   def q_mean_variance(self, x_start, t):
-    mean = self._extract(self.sqrt_alphas_cumprod, t, x_start.shape) * x_start
+    """
+    The mean and variance value in Eqn. (4)
+    """
+    # the mean value in right of Eqn. (4)
+    mean = self._extract(self.sqrt_alphas_cumprod, t, x_start.shape) * x_start 
+    # the variance value in right part of Eqn. (4)
     variance = self._extract(1. - self.alphas_cumprod, t, x_start.shape)
+    # the log value of variance
     log_variance = self._extract(self.log_one_minus_alphas_cumprod, t, x_start.shape)
     return mean, variance, log_variance
 
   def q_sample(self, x_start, t, noise=None):
     """
-    Diffuse the data (t == 0 means diffused for 1 step)
+    Diffuse the data (t == 0 means diffused for 1 step) relation below eqn. (8)
     """
-    if noise is None:
+    if noise is None: # noise refer to the \varepsilon in relation below Eqn. (8)
+      # if noise is not given, generate noise from tf.random_normal in the same shape of x_start
       noise = tf.random_normal(shape=x_start.shape)
-    assert noise.shape == x_start.shape
-    return (
+    assert noise.shape == x_start.shape # comfirm x_0 and noise are in the same shape
+    # x_t(x_0, \varepsilon) = \sqrt{\bar{\alpha}_t} x_0 + \sqrt{1 - \bar{\alpha}_t} \varepsilon
+    return ( 
         self._extract(self.sqrt_alphas_cumprod, t, x_start.shape) * x_start +
         self._extract(self.sqrt_one_minus_alphas_cumprod, t, x_start.shape) * noise
     )
 
-  def predict_start_from_noise(self, x_t, t, noise):
-    assert x_t.shape == noise.shape
-    return (
+  def predict_start_from_noise(self, x_t, t, noise): #the reverse fomula of q_sample above
+    assert x_t.shape == noise.shape # comfirm x_t and noise are in the same shape
+    return (  # the value in equation (10), x_0
         self._extract(self.sqrt_recip_alphas_cumprod, t, x_t.shape) * x_t -
         self._extract(self.sqrt_recipm1_alphas_cumprod, t, x_t.shape) * noise
     )
 
-  def q_posterior(self, x_start, x_t, t):
+  def q_posterior(self, x_start, x_t, t): # return the mean and varaince in Eqn. (6) 
     """
     Compute the mean and variance of the diffusion posterior q(x_{t-1} | x_t, x_0)
     """
@@ -142,21 +150,21 @@ class GaussianDiffusion:
     """
     Training loss calculation
     """
-    B, H, W, C = x_start.shape.as_list()
-    assert t.shape == [B]
+    B, H, W, C = x_start.shape.as_list() # obtain the shape of x_0
+    assert t.shape == [B] # comfirm the length of t and batch of x_0 are in the same value
 
-    if noise is None:
+    if noise is None: # if noise is empty, generate varepsilon from tf.random_normal in the same shape of x_0
       noise = tf.random_normal(shape=x_start.shape, dtype=x_start.dtype)
-    assert noise.shape == x_start.shape and noise.dtype == x_start.dtype
-    x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)
-    x_recon = denoise_fn(x_noisy, t)
+    assert noise.shape == x_start.shape and noise.dtype == x_start.dtype # check the noise and x_0 
+    x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise) # compute x_t(x_0, t) in Eqn. (10)
+    x_recon = denoise_fn(x_noisy, t)  # extract the noise from x_t(x_0, t)
     assert x_noisy.shape == x_start.shape
     assert x_recon.shape[:3] == [B, H, W] and len(x_recon.shape) == 4
 
     if self.loss_type == 'noisepred':
       # predict the noise instead of x_start. seems to be weighted naturally like SNR
       assert x_recon.shape == x_start.shape
-      losses = nn.meanflat(tf.squared_difference(noise, x_recon))
+      losses = nn.meanflat(tf.squared_difference(noise, x_recon)) # calculate the noise
     else:
       raise NotImplementedError(self.loss_type)
 
@@ -165,28 +173,28 @@ class GaussianDiffusion:
 
   def p_mean_variance(self, denoise_fn, *, x, t, clip_denoised: bool):
     if self.loss_type == 'noisepred':
-      x_recon = self.predict_start_from_noise(x, t=t, noise=denoise_fn(x, t))
+      x_recon = self.predict_start_from_noise(x, t=t, noise=denoise_fn(x, t)) # the predicted x_0, see in Eqn. (10)
     else:
       raise NotImplementedError(self.loss_type)
 
     if clip_denoised:
-      x_recon = tf.clip_by_value(x_recon, -1., 1.)
+      x_recon = tf.clip_by_value(x_recon, -1., 1.) #Clips tensor values to -1.0 and 1.0 #NOTE clip function cannot change the type of value automaticallty
 
-    model_mean, posterior_variance, posterior_log_variance = self.q_posterior(x_start=x_recon, x_t=x, t=t)
+    model_mean, posterior_variance, posterior_log_variance = self.q_posterior(x_start=x_recon, x_t=x, t=t) # Eqn. (7)
     assert model_mean.shape == x_recon.shape == x.shape
     assert posterior_variance.shape == posterior_log_variance.shape == [x.shape[0], 1, 1, 1]
-    return model_mean, posterior_variance, posterior_log_variance
+    return model_mean, posterior_variance, posterior_log_variance # Eqn. (7)
 
   def p_sample(self, denoise_fn, *, x, t, noise_fn, clip_denoised=True, repeat_noise=False):
     """
     Sample from the model
     """
     model_mean, _, model_log_variance = self.p_mean_variance(denoise_fn, x=x, t=t, clip_denoised=clip_denoised)
-    noise = noise_like(x.shape, noise_fn, repeat_noise)
+    noise = noise_like(x.shape, noise_fn, repeat_noise) # create noise, \varepsilon
     assert noise.shape == x.shape
     # no noise when t == 0
-    nonzero_mask = tf.reshape(1 - tf.cast(tf.equal(t, 0), tf.float32), [x.shape[0]] + [1] * (len(x.shape) - 1))
-    return model_mean + nonzero_mask * tf.exp(0.5 * model_log_variance) * noise
+    nonzero_mask = tf.reshape(1 - tf.cast(tf.equal(t, 0), tf.float32), [x.shape[0]] + [1] * (len(x.shape) - 1)) # when t==0, mask==0
+    return model_mean + nonzero_mask * tf.exp(0.5 * model_log_variance) * noise # the x_{t-1}, see the second line below Eqn. (11)
 
   def p_sample_loop(self, denoise_fn, *, shape, noise_fn=tf.random_normal):
     """
@@ -195,18 +203,18 @@ class GaussianDiffusion:
     i_0 = tf.constant(self.num_timesteps - 1, dtype=tf.int32)
     assert isinstance(shape, (tuple, list))
     img_0 = noise_fn(shape=shape, dtype=tf.float32)
-    _, img_final = tf.while_loop(
-      cond=lambda i_, _: tf.greater_equal(i_, 0),
+    _, img_final = tf.while_loop(  # Repeat body while the condition cond is true
+      cond=lambda i_, _: tf.greater_equal(i_, 0), #run until T==0
       body=lambda i_, img_: [
-        i_ - 1,
-        self.p_sample(denoise_fn=denoise_fn, x=img_, t=tf.fill([shape[0]], i_), noise_fn=noise_fn)
+        i_ - 1,     
+        self.p_sample(denoise_fn=denoise_fn, x=img_, t=tf.fill([shape[0]], i_), noise_fn=noise_fn)#sample x_{t-1}
       ],
       loop_vars=[i_0, img_0],
       shape_invariants=[i_0.shape, img_0.shape],
       back_prop=False
     )
     assert img_final.shape == shape
-    return img_final
+    return img_final 
 
   def p_sample_loop_trajectory(self, denoise_fn, *, shape, noise_fn=tf.random_normal, repeat_noise_steps=-1):
     """
